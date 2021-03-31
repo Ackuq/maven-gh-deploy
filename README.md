a8929eeb5ec2dcb20a4611637cd0bb6798c1b098
Ackuq@ <axel@pettersson.cc>
2021-03-31 17:26:48 +0200

    Add utility functions to library

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. Create .gitignore
 2. Run `mvn archetype:generate -DgroupId=com.kth.dd2482.utils -DartifactId=string-array-utils -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false`
+3. Create basic utility function code
---
+++ string-array-utils/src/main/java/com/kth/dd2482/utils/StringArrayUtils.java
@@ -0,0 +1,79 @@
+package com.kth.dd2482.utils;
+
+/**
+ * A class that provides simple utility functions that can be used on string
+ * arrays.
+ */
+public class StringArrayUtils {
+   /**
+    * Trim each element in the array
+    * 
+    * @param array An array of values
+    * @return The resulting array
+    */
+   public static String[] trim(String[] array) {
+      String[] result = new String[array.length];
+
+      for (int i = 0; i < array.length; i++) {
+         String value = array[i];
+         if (value != null) {
+            result[i] = value.trim();
+         }
+      }
+      return result;
+   }
+
+   /**
+    * Capitalize each element in the array
+    * 
+    * @param array An array of values
+    * @return The resulting array
+    */
+   public static String[] capitalize(String[] array) {
+      String[] result = new String[array.length];
+
+      for (int i = 0; i < array.length; i++) {
+         String value = array[i];
+         if (value != null) {
+            result[i] = value.substring(0, 1).toUpperCase() + value.substring(1);
+         }
+      }
+      return result;
+   }
+
+   /**
+    * Transforms the strings in the array to be lowercase
+    * 
+    * @param array An array of values
+    * @return The resulting array
+    */
+   public static String[] toLowerCase(String[] array) {
+      String[] result = new String[array.length];
+
+      for (int i = 0; i < array.length; i++) {
+         String value = array[i];
+         if (value != null) {
+            result[i] = value.toLowerCase();
+         }
+      }
+      return result;
+   }
+
+   /**
+    * Transforms the strings in the array to be uppercase
+    * 
+    * @param array An array of values
+    * @return The resulting array
+    */
+   public static String[] toUpperCase(String[] array) {
+      String[] result = new String[array.length];
+
+      for (int i = 0; i < array.length; i++) {
+         String value = array[i];
+         if (value != null) {
+            result[i] = value.toUpperCase();
+         }
+      }
+      return result;
+   }
+}
