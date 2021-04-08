e24c4c0f2393654eb48cfa4692df4eb6fdee88c2
Ackuq@ <axel@pettersson.cc>
2021-04-08 16:51:08 +0200

    Ad script for bumping versions

---
+++ string-array-utils/bump_version.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+new_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+case $1 in
+    "MAJOR")
+        # Get and increment major
+        new_major_value=$((${new_version:0:1} + 1))
+        # Replace the 1st value with our new value
+        new_version=$(echo $new_version | sed s/./$new_major_value/1)
+        ;;
+    "MINOR")
+        # Get and increment minor
+        new_minor_value=$((${new_version:2:1} + 1))
+        # Replace the 3rd value with our new value
+        new_version=$(echo $new_version | sed s/./$new_minor_value/3)
+        ;;
+    "PATCH")
+        # Get and increment patch
+        new_patch_value=$((${new_version:4:1} + 1))
+        # Replace the 5th value with our new value
+        new_version=$(echo $new_version | sed s/./$new_patch_value/5)
+        ;;
+    *)
+        echo "First argument should be one of 'MAJOR', 'MINOR', or 'PATCH'"
+        exit 1
+esac
+
+echo "Setting new version: $new_version"
+
+mvn versions:set -DnewVersion=$new_version
+
+if [[ $PRODUCTION == 'true' ]]; then
+    echo "PROD"
+    git add ./pom.xml
+    git commit -m "Release: $new_version"
+    git push
+    git push --tags
+fi
