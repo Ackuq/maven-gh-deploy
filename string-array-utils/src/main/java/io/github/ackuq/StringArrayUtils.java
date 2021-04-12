99015aafc5563c486c9b9d116ca5137cc525979f
Felix Seifert <mail@felix-seifert.com>
2021-04-12 08:25:46 +0200

    Streamline Java Code With Streams
    * Replace loops with streams which are collected in new arrays
    * Specify params in Javadocs

--- string-array-utils/src/main/java/io/github/ackuq/StringArrayUtils.java
+++ string-array-utils/src/main/java/io/github/ackuq/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package io.github.ackuq;
 
+import java.util.Arrays;
+
 /**
  * A class that provides simple utility functions that can be used on string
  * arrays.
@@ -8,72 +10,48 @@ public class StringArrayUtils {
    /**
     * Trim each element in the array
     * 
-    * @param array An array of values
+    * @param array An array of values to be trimmed
     * @return The resulting array
     */
    public static String[] trim(String[] array) {
-      String[] result = new String[array.length];
-
-      for (int i = 0; i < array.length; i++) {
-         String value = array[i];
-         if (value != null) {
-            result[i] = value.trim();
-         }
-      }
-      return result;
+      return Arrays.stream(array)
+              .map(s -> s != null ? s.trim() : null)
+              .toArray(String[]::new);
    }
 
    /**
     * Capitalize each element in the array
     * 
-    * @param array An array of values
+    * @param array An array of values to be capitalized
     * @return The resulting array
     */
    public static String[] capitalize(String[] array) {
-      String[] result = new String[array.length];
-
-      for (int i = 0; i < array.length; i++) {
-         String value = array[i];
-         if (value != null) {
-            result[i] = value.substring(0, 1).toUpperCase() + value.substring(1);
-         }
-      }
-      return result;
+      return Arrays.stream(array)
+              .map(s -> s !=null ? s.substring(0, 1).toUpperCase() + s.substring(1) : null)
+              .toArray(String[]::new);
    }
 
    /**
     * Transforms the strings in the array to be lowercase
     * 
-    * @param array An array of values
+    * @param array An array of values which should be transformed
     * @return The resulting array
     */
    public static String[] toLowerCase(String[] array) {
-      String[] result = new String[array.length];
-
-      for (int i = 0; i < array.length; i++) {
-         String value = array[i];
-         if (value != null) {
-            result[i] = value.toLowerCase();
-         }
-      }
-      return result;
+      return Arrays.stream(array)
+              .map(s -> s != null ? s.toLowerCase() : null)
+              .toArray(String[]::new);
    }
 
    /**
     * Transforms the strings in the array to be uppercase
     * 
-    * @param array An array of values
+    * @param array An array of values which should be transformed
     * @return The resulting array
     */
    public static String[] toUpperCase(String[] array) {
-      String[] result = new String[array.length];
-
-      for (int i = 0; i < array.length; i++) {
-         String value = array[i];
-         if (value != null) {
-            result[i] = value.toUpperCase();
-         }
-      }
-      return result;
+      return Arrays.stream(array)
+              .map(s -> s != null ? s.toUpperCase() : null)
+              .toArray(String[]::new);
    }
 }
